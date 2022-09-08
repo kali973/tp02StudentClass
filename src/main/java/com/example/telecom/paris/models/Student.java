2fa321b7ef9d0ddd81acd5c0a3028908f3a69f39
coutard <gilles.coutard@telecom-paris.fr>
2022-09-08 15:31:33 -0400

    Initialisation TP02 Telecom Paris CYBER701 - Java

--- src/main/java/com/example/telecom/paris/models/Student.java
+++ src/main/java/com/example/telecom/paris/models/Student.java
@@ -4,7 +4,6 @@ public class Student {
     private final int id;
     private String firstName;
     private String lastName;
-
     public Student(int id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
